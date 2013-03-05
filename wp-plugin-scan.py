1aca426367e3250128055c0a76912d93e35297d0
Anton Nizhegorodov <hookman.ru@gmail.com>
2013-03-05 12:51:47 +0200

    Add _isWebsiteAlive(url), scan(url); _isUrl() typo fix

--- wp-plugin-scan.py
+++ wp-plugin-scan.py
@@ -3,10 +3,11 @@
 import argparse
 import sys
 import re
+import urllib
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument('-s', '--scan', metavar='<website url>', help='scan website at <website url>')
-argParser.add_argument('-u', '--update', type=int, metavar='<page number>', help='update the list of plugins from wordpress.org up to <page number>')
+argParser.add_argument('-s', '--scan', metavar='<website url>', dest='url', help='scan website at <website url>')
+argParser.add_argument('-u', '--update', type=int, metavar='<page number>', dest='pageN', help='update the list of plugins from wordpress.org up to <page number>')
 args = argParser.parse_args()
 
 def _argumentsNumber():
@@ -14,7 +15,27 @@ def _argumentsNumber():
 
 def _isUrl(url):
 	pattern = re.compile('^https?://[\w\d\-\.]+/(([\w\d\-]+/)+)?$')
-	if p.match(url):
+	if pattern.match(url):
 		return True
 	else:
 		return False
+
+def _isWebsiteAlive(url):
+	try:
+		if urllib.urlopen(url).getcode() == 200:
+			print url + ' is alive...'
+		else:
+			print url + ' seems to be down...'
+	except IOError as e:
+		print e
+
+def scan(url):
+	try:
+		plugins = open('plugins.txt')
+		for line in plugins.read().split('\n'):
+			if line:
+				code = urllib.urlopen(url + 'wp-content/plugins/' + line + '/').getcode()
+				if code != 404:
+					print line + '[+]'
+	except IOError as e:
+		print e
