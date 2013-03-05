bbb2e1745e7401aaf137ffb21ee8660eaa5564a1
Anton Nizhegorodov <hookman.ru@gmail.com>
2013-03-05 16:46:03 +0200

    Add update(pageN), _parseHrefs(html), _writePlugins(pluginsList)

--- wp-plugin-scan.py
+++ wp-plugin-scan.py
@@ -4,6 +4,8 @@ import argparse
 import sys
 import re
 import urllib
+import lxml.html
+import os
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument('-s', '--scan', metavar='<website url>', dest='url', help='scan website at <website url>')
@@ -30,12 +32,46 @@ def _isWebsiteAlive(url):
 		print e
 
 def scan(url):
+	currentDir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
 	try:
-		plugins = open('plugins.txt')
-		for line in plugins.read().split('\n'):
+		pluginsFile = open(currentDir + 'plugins.txt', 'r')
+		for line in pluginsFile.read().split('\n'):
 			if line:
 				code = urllib.urlopen(url + 'wp-content/plugins/' + line + '/').getcode()
 				if code != 404:
 					print line + '[+]'
 	except IOError as e:
 		print e
+
+def update(pageN):
+	pluginsList = []
+	if pageN == 1:
+		html = urllib.urlopen('http://wordpress.org/extend/plugins/browse/popular/').read()
+		pluginsList = _parseHrefs(html)
+	elif pageN == 2:
+		html = urllib.urlopen('http://wordpress.org/extend/plugins/browse/popular/').read()
+		pluginsList = _parseHrefs(html)
+		html = urllib.urlopen('http://wordpress.org/extend/plugins/browse/popular/page/2/').read()
+		pluginsList = pluginsList + _parseHrefs(html)
+	else:
+		for page in range(2, pageN):
+			html = urllib.urlopen('http://wordpress.org/extend/plugins/browse/popular/page/' + str(pageN) + '/').read()
+			pluginsList = pluginsList + _parseHrefs(html)
+	_writePlugins(pluginsList)
+
+def _parseHrefs(html):
+	doc = lxml.html.document_fromstring(html)
+	pattern = re.compile('/plugins/([\w\d\-]+)/')
+	pluginsList = []
+	links = doc.cssselect('div.plugin-block h3 a')
+	for link in links:
+		plugin = pattern.search(link.get('href')).group(1)
+		pluginsList.append(plugin)
+		print plugin + '[+]'
+	return pluginsList
+
+def _writePlugins(pluginsList):
+	currentDir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
+	pluginsFile = open(currentDir + 'plugins.txt', 'w')
+	pluginsFile.write('\n'.join(pluginsList))
+	pluginsFile.close()
